9c40bad976a2936d728995ec466595ad2a889de6
walfud <hi@walfud.com>
2018-02-01 14:47:04 +0800

    jest

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ const fromString = (function () {
     }
 })()
 
-module.export = {
+module.exports = {
     fromNumber,
     fromString,
 }
---
+++ index.test.js
@@ -0,0 +1,15 @@
+const {
+    fromNumber,
+    fromString,
+} = require('./index')
+
+test('fromNumber', () => {
+    expect(eval(fromNumber(0))).toBe(0)
+    expect(eval(fromNumber(1))).toBe(1)
+    expect(eval(fromNumber(-1))).toBe(-1)
+    expect(eval(fromNumber(1942))).toBe(1942)
+})
+
+test('fromString', () => {
+    expect(eval(fromString('`1234567890-=qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>?'))).toBe('`1234567890-=qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>?')
+})
