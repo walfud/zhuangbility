b4540c476a916183e1843bb7e1ebc1bb53ad9f49
walfud <hi@walfud.com>
2018-02-01 14:14:54 +0800

    refactor generate char map

--- index.js
+++ index.js
@@ -38,66 +38,61 @@ const fromNumber = (function () {
 })()
 
 const fromString = (function () {
-    const _false = '[]+![]',      // 'false'
-        _true = '[]+!![]',        // 'true'
-        _undefined = '[]+[][[]]', // 'undefined'
-        _NaN = `[]+(+${_undefined})`,   // 'NaN'
-        _objectObject = '[]+{}'   // '[object Object]'
-    const char = {
-        a: `(${_false})[${fromNumber(1)}]`,
-        b: `(${_objectObject})[${fromNumber(2)}]`,
-        c: `(${_objectObject})[${fromNumber(5)}]`,
-        d: `(${_undefined})[${fromNumber(2)}]`,
-        e: `(${_false})[${fromNumber(4)}]`,
-        f: `(${_false})[${fromNumber(0)}]`,
-        i: `(${_undefined})[${fromNumber(5)}]`,
-        j: `(${_objectObject})[${fromNumber(3)}]`,
-        l: `(${_false})[${fromNumber(2)}]`,
-        n: `(${_undefined})[${fromNumber(1)}]`,
-        o: `(${_objectObject})[${fromNumber(1)}]`,
-        r: `(${_true})[${fromNumber(1)}]`,
-        s: `(${_false})[${fromNumber(3)}]`,
-        t: `(${_true})[${fromNumber(0)}]`,
-        u: `(${_true})[${fromNumber(2)}]`,
-        N: `(${_NaN})[${fromNumber(1)}]`,
-        ' ': `(${_objectObject})[${fromNumber(7)}]`,
-    };
+    // Generate base char map
+    const char = {};
+    [
+        '[]+{}',            // '[object Object]'
+        '[]+![]',           // 'false'
+        '[]+!![]',          // 'true'
+        '[]+[][[]]',        // 'undefined'
+        '[]+(+[]+[][[]])',  // 'NaN'
+    ].forEach(s =>
+        eval(s)
+            .split('')
+            .map((c, i) => {
+                const _ = `(${s})[${fromNumber(i)}]`
+                if (!char.hasOwnProperty(c)
+                    || char[c].length > _.length) {
+                    char[c] = _
+                }
+            })
+        )
+    for (let i = 0; i < 10; i++) {
+        char[i] = `[]+(${fromNumber(i)})`
+    }
 
-    function transform(map, code) {
+    function transform(code) {
         return code
             .split('')
             .map(c => {
-                if (!map.hasOwnProperty(c)) {
+                if (!char.hasOwnProperty(c)) {
                     throw new Error(`fromString not support: '${c}'`)
                 }
-                return `(${map[c]})`
+                return `(${char[c]})`
             })
             .join('+')
     }
 
+    // Get character for `toString`
     // ''+''['constructor'] => 'function String() { [native code] }'
-    const _functionString = `[]+([]+[])[${transform(char, 'constructor')}]`
+    const _functionString = `[]+([]+[])[${transform('constructor')}]`
     char.g = `(${_functionString})[${fromNumber(14)}]`
     char.v = `(${_functionString})[${fromNumber(25)}]`
     char.S = `(${_functionString})[${fromNumber(9)}]`
     char['('] = `(${_functionString})[${fromNumber(15)}]`
     char[')'] = `(${_functionString})[${fromNumber(16)}]`
 
-    // Fill all lowercase char
+    // Fill all lowercase char using `(Number).toString`
     for (let i = 10; i < 36; i++) {
         const c = i.toString(36)
         if (!char.hasOwnProperty(c)) {
             // i['toString'](36)
-            char[c] = `(${fromNumber(i)})[${transform(char, 'toString')}](${fromNumber(36)})`
+            char[c] = `(${fromNumber(i)})[${transform('toString')}](${fromNumber(36)})`
         }
     }
 
     // Fill other with `unescape`
     // There should be an other way because `unescape` is deprecated and will be remove in future
-    const allMap = {
-        ...char,
-        ...'0123456789'.split('').map(i => `[]+(${fromNumber(i)})`),
-    }
     for (let i = 32; i < 128; i++) {
         const hexStr = i.toString(16)
         const c = unescape(`%${hexStr}`)
@@ -106,11 +101,11 @@ const fromString = (function () {
         }
 
         // []['sort']['constructor']('return unescape')()
-        char[c] = `[][${transform(char, 'sort')}][${transform(char, 'constructor')}](${transform(allMap, 'return unescape')})()('%'+(${transform(allMap, hexStr)}))`
+        char[c] = `[][${transform('sort')}][${transform('constructor')}](${transform('return unescape')})()('%'+(${transform(hexStr)}))`
     }
 
     return function (str) {
-        return transform(char, str)
+        return transform(str)
     }
 })()
 
